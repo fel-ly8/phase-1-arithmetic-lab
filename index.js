d604db2683f016b55420b596c24e98931e95ef83
felister kiragu <kiragufelister18@gmail.com>
2024-09-27 11:42:03 +0300

    add index.html

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+// question 1
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2;
+console.log(multiply);
+// question 2
+
+let random = Math.floor(Math.random() *100) +1
+console.log(random)
+// question 3
+let num3 = 44
+let num4 = 20
+let mod = num3 % num4
+console.log(mod)
+//  question 4
+let max = 20
+console.log(max);
+
+
